c60a7ce2980223efd81b2117d78c35ec4b773e03
Nick Fehlinger <nfehlinger@gmail.com>
2017-06-24 23:51:48 -0400

    Started building a foundation for us to work on (layout, some suby, js, sass, etc) Details are in a newly added ReadMe which I highly encourage you to check out.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
+// Universal
+document.querySelector("a[href='" + window.location.pathname + "']").className="active"
+
+year = new Date().getFullYear()
+document.querySelector("#year").innerHTML = year
+
 // Nick
 
 
