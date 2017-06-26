d3939bafa2faf3789032073989b0988a38d89f8c
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-06-26 18:06:44 -0400

    new version

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ function carousel() {
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    x[myIndex-1].style.display = "inline-block";  
     setTimeout(carousel, 2000); 
 }
 
